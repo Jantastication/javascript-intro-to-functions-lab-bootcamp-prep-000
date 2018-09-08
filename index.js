f750826494a6bef190c1ee687a204ee02e4c7d2a
Jantastication O <georginaoregx@gmail.com>
2018-09-08 00:33:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,12 +18,11 @@ function logWhisper(string) {
 logWhisper('HELLO')
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase())
+  if (string.toLowerCase() === string) {
   return "I can\'t hear you!";
-} else if (string.toLowerCase())===string {
+  } else if (string.toUpperCase() === string) {
   return "YES INDEED!";
-} else if (string === "I love you, Grandma.")) {
+  } else if (string === "I love you, Grandma.") {
   return "I love you, too.";
-}
+  }
 };
-
